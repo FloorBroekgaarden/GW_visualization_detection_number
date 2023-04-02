1066e3d19050a2c1bfefddb09699c5b1cb6d7b2d
FloorBroekgaarden <floor.broekgaarden@cfa.harvard.edu>
2023-04-02 11:48:50 -0400

    update with cleaning github dir

--- makeMovie.py
+++ makeMovie.py
@@ -14,14 +14,14 @@ import glob
 
 
 
-def makeMovie_GWcatalogSize(fps=20, duration=60, name_images='none', number_images=400):
+def makeMovie_GWcatalogSize(fps=20, duration=60, name_images='none', number_images=400, image_folder='./GW_visualization_detection_number/'):
 	'''
 	whichRate = 'intrinsic' or 'observed'
 	fps=0.4, frames per second
 	duration = duration of the movie 
 	'''
 
-	image_folder = '/Users/floorbroekgaarden/Projects/GitHub/Othercode/GWcatalogMovie/'
+	
 
 	images = []
 
@@ -35,39 +35,24 @@ def makeMovie_GWcatalogSize(fps=20, duration=60, name_images='none', number_imag
 	clip.write_videofile(image_folder+'movie_'+ name_images  + '.mp4')
 
 
-	# make also gif:
- 
-	# Create the frames
-	frames = []
-	# imgs = glob.glob("*.png")
-	for i in images:
-	    new_frame = Image.open(i)
-	    frames.append(new_frame)
-	 
-	# Save into a GIF file that loops forever
-	frames[0].save(image_folder+'gif_'+ name_images +  '.gif', format='GIF',
-	               append_images=frames[1:],
-	               save_all=True,
-	               duration=duration, loop=0)
-
 
 	print('done')
 	return 
 
 
 
-def makeMovie_GWcatalogSizeJustConfirmed(fps=20, duration=60, name_images='none'):
+def makeMovie_GWcatalogSizeJustConfirmed(fps=20, duration=60, name_images='none', image_folder='/Users/floorbroekgaarden/Projects/GitHub/GW_visualization_detection_number/GWcatalogFigures/'):
 	'''
 	whichRate = 'intrinsic' or 'observed'
 	fps=0.4, frames per second
 	duration = duration of the movie 
 	'''
 
-	image_folder = '/Users/floorbroekgaarden/Projects/GitHub/Othercode/GWcatalogFigures/'
+
 
 	images = []
 
-	for ind_im  in range(4):
+	for ind_im  in range(5):
 			images.append(image_folder +   name_images  + str(ind_im) + '.png')
 
 
@@ -77,36 +62,20 @@ def makeMovie_GWcatalogSizeJustConfirmed(fps=20, duration=60, name_images='none'
 	clip.write_videofile(image_folder+'movie_'+ name_images  + '.mp4')
 
 
-	# make also gif:
- 
-	# Create the frames
-	frames = []
-	# imgs = glob.glob("*.png")
-	for i in images:
-	    new_frame = Image.open(i)
-	    frames.append(new_frame)
-	 
-	# Save into a GIF file that loops forever
-	frames[0].save(image_folder+'gif_'+ name_images +  '.gif', format='GIF',
-	               append_images=frames[1:],
-	               save_all=True,
-	               duration=duration, loop=0)
-
-
 	print('done')
 	return 
 
 
 
 
-def makeMovie_GWcatalogSizeO4(fps=60, duration=50):
+def makeMovie_GWcatalogSizeO4(fps=60, duration=50, image_folder = '/Users/floorbroekgaarden/Projects/GitHub/GW_visualization_detection_number/GWcatalogMovie/'):
 	'''
 	whichRate = 'intrinsic' or 'observed'
 	fps=0.4, frames per second
 	duration = duration of the movie 
 	'''
 
-	image_folder = '/Users/floorbroekgaarden/Projects/GitHub/Othercode/GWcatalogMovie/'
+	
 
 	images = []
 
@@ -137,51 +106,6 @@ def makeMovie_GWcatalogSizeO4(fps=60, duration=50):
 
 
 
-def makeMovie_GWcatalogSize_log(fps=20, duration=60):
-	'''
-	whichRate = 'intrinsic' or 'observed'
-	fps=0.4, frames per second
-	duration = duration of the movie 
-	'''
-
-
-
-
-  
-	image_folder = '/Users/floorbroekgaarden/Projects/GitHub/Othercode/GWcatalogMovie/'
-
-	images = []
-
-	for ind_im  in range(200):
-			images.append(image_folder +   'GWcatalogSize_log_'  + str(ind_im) + '.png')
-
-
-
-	image_files = images
-	clip = moviepy.video.io.ImageSequenceClip.ImageSequenceClip(image_files, fps=fps)
-	clip.write_videofile(image_folder+'movie_'+ 'GWcatalogSize_log'  + '.mp4')
-
-
-	# make also gif:
- 
-	# Create the frames
-	frames = []
-	# imgs = glob.glob("*.png")
-	for i in images:
-	    new_frame = Image.open(i)
-	    frames.append(new_frame)
-	 
-	# Save into a GIF file that loops forever
-	frames[0].save(image_folder+'gif_'+ 'GWcatalogSize_log' +  '.gif', format='GIF',
-	               append_images=frames[1:],
-	               save_all=True,
-	               duration=duration, loop=0)
-
-
-	print('done')
-	return 
-
-
 
 
 
@@ -193,20 +117,24 @@ def makeMovie_GWcatalogSize_log(fps=20, duration=60):
 Movie_GWcatalogSize=True
 
 
-
 # Run this using python 3!! 
-
 if Movie_GWcatalogSize==True:
+	
+	# # this just makes a slide show (slow movie) of the 5 catalogs 
+	# image_folder='/Users/floorbroekgaarden/Projects/GitHub/GW_visualization_detection_number/GWcatalogFigures/'
+	# makeMovie_GWcatalogSizeJustConfirmed(fps=1.2, duration=200, name_images='knownDetectionsGWcatalogSize_', image_folder=image_folder)
+	
+	image_folder = '/Users/floorbroekgaarden/Projects/GitHub/GW_visualization_detection_number/GWcatalogMovie/'
+	# makeMovie_GWcatalogSize(fps=45, duration=40, name_images='knownDetectionsGWcatalogSizeMovie_', image_folder=image_folder) # make movie different O1-O3 catalogs 
+	# # makes movie lineaer y-scale and without uncertainties:
+	# makeMovie_GWcatalogSize(fps=20, duration=60, name_images='GWcatalogSize_withUpdatedCE_', number_images=200, image_folder=image_folder)
+	# # makes movie lineaer y-scale and with uncertainties:
+	# makeMovie_GWcatalogSize(fps=20, duration=60, name_images='GWcatalogSize_UncertaintyLin_', number_images=200, image_folder=image_folder)
+	# # make movies log y-scale and with uncertainties:
+	# makeMovie_GWcatalogSize(fps=20, duration=60, name_images='GWcatalogSize_withUncertainty_', number_images=200, image_folder=image_folder)
+	# make movie for O4
+	makeMovie_GWcatalogSizeO4(image_folder=image_folder)
 
-	# makeMovie_GWcatalogSizeJustConfirmed(fps=1.2, duration=200, name_images='knownDetectionsGWcatalogSize_')
-	# # makeMovie_GWcatalogSize(fps=45, duration=40, name_images='knownDetectionsGWcatalogSizeMovie_') # make movie different O1-O3 catalogs 
-	# makeMovie_GWcatalogSize(fps=20, duration=60, name_images='GWcatalogSize_UncertaintyLin_', number_images=200)
-	# makeMovie_GWcatalogSize(fps=20, duration=60, name_images='GWcatalogSize_withUpdatedCE_', number_images=200)
-	# makeMovie_GWcatalogSize(fps=20, duration=60, name_images='GWcatalogSize_withUncertainty_', number_images=200)
-
-	makeMovie_GWcatalogSizeO4()
-	# makeMovie_GWcatalogSize()
-	# makeMovie_GWcatalogSize_log()
 
 
 
